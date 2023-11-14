274a05a888c3f72849ccfd6c21edb452ab3d2065
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-14 13:37:59 -0800

    54. fresh start

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,19 +2,23 @@ import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsPending(true);
+
       const res = await fetch(url);
       const json = await res.json();
 
+      setIsPending(false);
       setData(json);
     };
 
     fetchData();
   }, [url]);
 
-  return { data };
+  return { data, isPending };
 };
 
 export default useFetch;
