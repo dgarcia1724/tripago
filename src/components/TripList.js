64d67bcdb13952c6139827d391e82631845e22eb
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-14 14:08:28 -0800

    56. fresh start

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -5,7 +5,7 @@ import useFetch from "../hooks/useFetch";
 import "./TripList.css";
 
 function TripList() {
-  const [url, setUrl] = useState("http://localhost:3000/tripsdas");
+  const [url, setUrl] = useState("http://localhost:3000/trips");
   const { data: trips, isPending, error } = useFetch(url);
 
   return (
